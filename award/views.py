ae221a7d8e38523d732cecc56c4535ce4fe5091e
Matt Perrott <mperrott6@gmail.com>
2017-05-09 11:45:56 +1000

    Change AwardList to Generic

--- award/views.py
+++ award/views.py
@@ -1,22 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import render
+from rest_framework import generics
 from serializers import AwardSerializer
 
 from award.models import Award
 
-class AwardList(APIView):
-    def get(self, request, format=None):
-        awards = Award.objects.all()
-        serializer = AwardSerializer(awards, many=True)
-        return Response(serializer.data)
-        
-    def post(self, request, format=None):
-        serializer = AwardSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AwardList(generics.ListCreateAPIView):
+    queryset = Award.objects.all()
+    serializer_class = AwardSerializer
