dd2d0f6d1934984d588fbbf608ee19f2c7d048cb
Matt Perrott <mperrott6@gmail.com>
2017-06-22 11:35:53 +1000

    Add app name to award urls

--- award/urls.py
+++ award/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 
 from award import views
 
+app_name = 'award'
 urlpatterns = [
-    url(r'^$', views.AwardList.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$',  views.AwardDetail.as_view()),
+    url(r'^$', views.AwardList.as_view(), name='list'),
+    url(r'^(?P<pk>[0-9]+)/$',  views.AwardDetail.as_view(), name='detail'),
 ]
